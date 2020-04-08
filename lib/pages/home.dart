9d70b0163ffbc7e53a97f7220d9a1b8975de3e9a
osamtimizer <eaten.jb@gmail.com>
2020-04-09 02:02:23 +0900

    show simple chart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,38 @@
 import 'package:covid_19_viewer/imports.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final covid19 = Provider.of<Covid19Store>(context).covid19;
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
       body: Center(
-        child: Text(covid19.transition.carriers.first.count.toString()),
+        child: _carriersChart(context),
       ),
     );
+  }
+
+  Widget _carriersChart(BuildContext context) {
+    final covid19 = Provider.of<Covid19Store>(context).covid19;
+    return charts.BarChart(
+      _createSeries(covid19.transition.carriers),
+      animate: true,
+    );
+  }
+
+  static List<charts.Series<Carrier, String>> _createSeries(
+      List<Carrier> carriers) {
+    final target = carriers.sublist(carriers.length - 28, carriers.length);
+    return [
+      charts.Series<Carrier, String>(
+        id: 'Carrier',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (carrier, count) => carrier.day.toString(),
+        measureFn: (carrier, count) => carrier.count,
+        data: target,
+      )
+    ];
   }
 }
