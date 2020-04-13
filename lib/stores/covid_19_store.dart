569876ee3c934487fa2ac76b3078825d5406fc63
osamtimizer <eaten.jb@gmail.com>
2020-04-14 01:32:10 +0900

    add isfetching flag

--- lib/stores/covid_19_store.dart
+++ lib/stores/covid_19_store.dart
@@ -5,6 +5,8 @@ class Covid19Store extends ChangeNotifier {
   Covid19 _covid19;
   Covid19 get covid19 => _covid19;
   int _selectedPrefectureCode = Constants.Tokyo;
+  bool _isFetching = false;
+  bool get isFetching => _isFetching;
 
   PrefecturesMap get selectedPrefecture {
     if (_covid19 == null || _selectedPrefectureCode <= 0) {
@@ -21,12 +23,18 @@ class Covid19Store extends ChangeNotifier {
   }
 
   void refreshCovid19() async {
+    if (_isFetching) {
+      return;
+    }
+
+    _isFetching = true;
     final request = Covid19Request(client: http.Client());
     request.fetch().then((value) {
       if (value != null) {
         _covid19 = value;
-        notifyListeners();
       }
+      _isFetching = false;
+      notifyListeners();
     });
   }
 
